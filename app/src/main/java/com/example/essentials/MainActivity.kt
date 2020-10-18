ab82a1ea351604844a24b84392bec8fa330664b5
ziggymoens <ziggy.moens@student.hogent.be>
2020-10-18 16:22:27 +0200

    Ktlint changes

--- app/src/main/java/com/example/essentials/MainActivity.kt
+++ app/src/main/java/com/example/essentials/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.example.essentials
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
