991ca2f4f8e5c9aaf4aa1517f28bda9f34d744be
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-27 21:16:03 +0100

    finish homescreen with navigation

--- app/src/main/java/com/example/essentials/MainActivity.kt
+++ app/src/main/java/com/example/essentials/MainActivity.kt
@@ -1,36 +1,38 @@
 package com.example.essentials
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.essentials.databinding.ActivityMainBinding
+import com.example.essentials.ui.changeInitiatives.ChangeInitiativesFragment
+import com.example.essentials.ui.dashboard.DashboardFragment
+import com.example.essentials.ui.surveyScreen.SurveyFragment
+import com.example.essentials.ui.teams.TeamsFragment
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
 
-    // TODO change title of appbar
-    private val _title = MutableLiveData<String>()
-    val title: LiveData<String>
-        get() = _title
-
-    fun updateActionBarTitle(title: String) = _title.postValue(title)
-    // TODO end
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 //        setContentView(R.layout.activity_main)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
         drawerLayout = binding.drawerLayout
 
         val navController = this.findNavController(R.id.navHostFragment)
@@ -44,9 +46,58 @@ class MainActivity : AppCompatActivity() {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
             }
         }
+
+        // TODO EXPERIMENTAL - make drawer navigate - does not work properly yet
+//        var nvDrawer = findViewById<NavigationView>(R.id.navView);
+//        // Setup drawer view
+//        setupDrawerContent(nvDrawer);
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
         return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
+
+    // TODO EXPERIMENTAL - make drawer navigate - does not work properly yet
+//    private fun setupDrawerContent(navigationView: NavigationView) {
+//        navigationView.setNavigationItemSelectedListener(
+//            object : NavigationView.OnNavigationItemSelectedListener {
+//                override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+//                    selectDrawerItem(menuItem)
+//                    return true
+//                }
+//            })
+//    }
+//
+//    fun selectDrawerItem(menuItem: MenuItem) {
+//        // Create a new fragment and specify the fragment to show based on nav item clicked
+//        var fragment: Fragment? = null
+//        val fragmentClass: Class<*>
+//        fragmentClass = when (menuItem.getItemId()) {
+//            R.id.changeInitiativesDrawer -> ChangeInitiativesFragment::class.java
+//            R.id.dashBoardDrawer -> DashboardFragment::class.java
+//            R.id.surveysDrawer -> SurveyFragment::class.java
+//            R.id.teamsDrawer -> TeamsFragment::class.java
+//            R.id.myChangeInitiativesDrawer -> ChangeInitiativesFragment::class.java //TODO veranderen naar de juiste fragment
+//            else -> ChangeInitiativesFragment::class.java
+//        }
+//        try {
+//            fragment = fragmentClass.newInstance() as Fragment
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//
+//        // Insert the fragment by replacing any existing fragment
+//        val fragmentManager: FragmentManager = supportFragmentManager
+//        fragment?.let {
+//            fragmentManager.beginTransaction().replace(R.id.navHostFragment,
+//                it
+//            ).commit()
+//        }
+//
+//        // Set action bar title
+//        setTitle(menuItem.getTitle())
+//        // Close the navigation drawer
+//        drawerLayout.closeDrawers()
+//    }
+
 }
