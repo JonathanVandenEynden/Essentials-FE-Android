0fcc3d21fba6fe2c093b92584966d8920eaabb60
ziggymoens <ziggy.moens@student.hogent.be>
2020-10-30 20:25:44 +0100

    Errors Google Play store fixed
    - Fragment changed to fragmentcontainerview (activity_main.xml)
    - safeargs import changed
    - navController init fixed in MainActivity.kt
    - Description for screenreader added to ratingBar in survey_question.xml

--- app/src/main/java/com/hogentessentials1/essentials/MainActivity.kt
+++ app/src/main/java/com/hogentessentials1/essentials/MainActivity.kt
@@ -5,7 +5,8 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
@@ -17,10 +18,11 @@ import com.hogentessentials1.essentials.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
-    private val navController by lazy {
-        Navigation.findNavController(this, R.id.navHostFragment)
-    }
+    /*private val navController by lazy {
+        this.findNavController(R.id.navHostFragment)
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +32,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.layout.activity_main
         )
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
         binding.navView.setupWithNavController(navController)
         NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
         binding.navView.setNavigationItemSelectedListener(this)
