6b4c7023e27c40920030d555a07bd033adc1ae3d
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-11-14 21:21:31 +0100

    Generate login activity add logo

---
+++ app/src/main/java/com/hogentessentials1/essentials/login/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.hogentessentials1.essentials.login.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    // ... other data fields that may be accessible to the UI
+)
