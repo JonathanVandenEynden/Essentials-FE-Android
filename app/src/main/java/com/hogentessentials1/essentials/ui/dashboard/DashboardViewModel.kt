a9195667775f508d6b87ce0b68d77667a413cf22
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-11-26 10:59:23 +0100

    versies aanpassen

--- app/src/main/java/com/hogentessentials1/essentials/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/hogentessentials1/essentials/ui/dashboard/DashboardViewModel.kt
@@ -22,93 +22,93 @@ class DashboardViewModel : ViewModel() {
 //        return surveys
 //    }
 
-    var changeInitiatives: ArrayList<ChangeInitiative> = arrayListOf(
-        ChangeInitiative(
-            title = "New Resto",
-            surveys = arrayListOf(
-                Survey(
-                    name = "Personnel Survey",
-                    arrayListOf(
-                        SurveyQuestion(
-                            question = "What do you think of the new personnel?",
-                            option0 = "Uneatable",
-                            option5 = "Delicious"
-                        ),
-                        SurveyQuestion(
-                            question = "Would you recommend the new resto to your colleagues?",
-                            option0 = "No",
-                            option5 = "Yes"
-                        )
-                    )
-                ),
-                Survey(
-                    name = "Food Survey",
-                    arrayListOf(
-                        SurveyQuestion(
-                            question = "What do you think of the new food?",
-                            option0 = "Uneatable",
-                            option5 = "Delicious"
-                        ),
-                        SurveyQuestion(
-                            question = "Would you recommend the food to your colleagues?",
-                            option0 = "No",
-                            option5 = "Yes"
-                        )
-                    )
-                ),
-                Survey(
-                    name = "Impact Survey",
-                    arrayListOf(
-                        SurveyQuestion(
-                            question = "What do you think this change has an impact on your work?",
-                            option0 = "No",
-                            option5 = "Yes"
-                        ),
-                        SurveyQuestion(
-                            question = "Would you feel more happy at work?",
-                            option0 = "No",
-                            option5 = "Yes"
-                        )
-                    )
-                )
-            )
-        ),
-        ChangeInitiative(
-            title = "Expansion to German market",
-            surveys = arrayListOf(
-                Survey(
-                    name = "Financial Survey",
-                    arrayListOf(
-                        SurveyQuestion(
-                            question = "What do you think of the new financial arrangement concerning the expansion?",
-                            option0 = "Very bad",
-                            option5 = "Very Good"
-                        ),
-                        SurveyQuestion(
-                            question = "Do you think you will get a raise by the end of the year?",
-                            option0 = "No",
-                            option5 = "Yes"
-                        )
-                    )
-                ),
-                Survey(
-                    name = "Work Survey",
-                    arrayListOf(
-                        SurveyQuestion(
-                            question = "Do you feel more pressure at work?",
-                            option0 = "No",
-                            option5 = "Yes"
-                        ),
-                        SurveyQuestion(
-                            question = "Would you like working form home while we are expanding?",
-                            option0 = "No",
-                            option5 = "Yes"
-                        )
-                    )
-                )
-            )
-        )
-    )
+    var changeInitiatives: ArrayList<ChangeInitiative> = arrayListOf()
+//        ChangeInitiative(
+//            title = "New Resto",
+//            surveys = arrayListOf(
+//                Survey(
+//                    name = "Personnel Survey",
+//                    arrayListOf(
+//                        SurveyQuestion(
+//                            question = "What do you think of the new personnel?",
+//                            option0 = "Uneatable",
+//                            option5 = "Delicious"
+//                        ),
+//                        SurveyQuestion(
+//                            question = "Would you recommend the new resto to your colleagues?",
+//                            option0 = "No",
+//                            option5 = "Yes"
+//                        )
+//                    )
+//                ),
+//                Survey(
+//                    name = "Food Survey",
+//                    arrayListOf(
+//                        SurveyQuestion(
+//                            question = "What do you think of the new food?",
+//                            option0 = "Uneatable",
+//                            option5 = "Delicious"
+//                        ),
+//                        SurveyQuestion(
+//                            question = "Would you recommend the food to your colleagues?",
+//                            option0 = "No",
+//                            option5 = "Yes"
+//                        )
+//                    )
+//                ),
+//                Survey(
+//                    name = "Impact Survey",
+//                    arrayListOf(
+//                        SurveyQuestion(
+//                            question = "What do you think this change has an impact on your work?",
+//                            option0 = "No",
+//                            option5 = "Yes"
+//                        ),
+//                        SurveyQuestion(
+//                            question = "Would you feel more happy at work?",
+//                            option0 = "No",
+//                            option5 = "Yes"
+//                        )
+//                    )
+//                )
+//            )
+//        ),
+//        ChangeInitiative(
+//            title = "Expansion to German market",
+//            surveys = arrayListOf(
+//                Survey(
+//                    name = "Financial Survey",
+//                    arrayListOf(
+//                        SurveyQuestion(
+//                            question = "What do you think of the new financial arrangement concerning the expansion?",
+//                            option0 = "Very bad",
+//                            option5 = "Very Good"
+//                        ),
+//                        SurveyQuestion(
+//                            question = "Do you think you will get a raise by the end of the year?",
+//                            option0 = "No",
+//                            option5 = "Yes"
+//                        )
+//                    )
+//                ),
+//                Survey(
+//                    name = "Work Survey",
+//                    arrayListOf(
+//                        SurveyQuestion(
+//                            question = "Do you feel more pressure at work?",
+//                            option0 = "No",
+//                            option5 = "Yes"
+//                        ),
+//                        SurveyQuestion(
+//                            question = "Would you like working form home while we are expanding?",
+//                            option0 = "No",
+//                            option5 = "Yes"
+//                        )
+//                    )
+//                )
+//            )
+//        )
+//    )
 
     init {
         // TODO hier data ophalen van API
