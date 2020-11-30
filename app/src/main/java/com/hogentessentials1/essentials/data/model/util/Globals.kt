e41bf0a2bac544b3516fd846c94e262a87cdd998
JonathanVandenEynden <47554911+JonathanVandenEynden@users.noreply.github.com>
2020-11-30 18:14:17 +0100

    BASE URL edited

--- app/src/main/java/com/hogentessentials1/essentials/data/model/util/Globals.kt
+++ app/src/main/java/com/hogentessentials1/essentials/data/model/util/Globals.kt
@@ -2,6 +2,6 @@ package com.hogentessentials1.essentials.data.model.util
 
 class Globals {
     companion object {
-        const val BASE_URL = "https://essentialsapi.azurewebsites.net/api/"
+        const val BASE_URL = "https://essentials-api.azurewebsites.net/api/"
     }
 }
