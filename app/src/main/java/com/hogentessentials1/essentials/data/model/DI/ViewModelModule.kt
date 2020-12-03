11c51a3ad1bb7d16961b43b0e866d456d6be5ce6
ziggymoens <ziggy.moens@student.hogent.be>
2020-12-03 12:51:38 +0100

    small fix

--- app/src/main/java/com/hogentessentials1/essentials/data/model/DI/ViewModelModule.kt
+++ app/src/main/java/com/hogentessentials1/essentials/data/model/DI/ViewModelModule.kt
@@ -4,6 +4,8 @@ import com.hogentessentials1.essentials.login.ui.login.LoginViewModel
 import com.hogentessentials1.essentials.ui.changeGroup.TeamsViewModel
 import com.hogentessentials1.essentials.ui.changeInitiatives.ChangeInitiativesViewModel
 import com.hogentessentials1.essentials.ui.roadMap.list.RoadMapListViewModel
+import com.hogentessentials1.essentials.ui.survey.SurveyDoneViewModel
+import com.hogentessentials1.essentials.ui.survey.SurveyQuestionViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
