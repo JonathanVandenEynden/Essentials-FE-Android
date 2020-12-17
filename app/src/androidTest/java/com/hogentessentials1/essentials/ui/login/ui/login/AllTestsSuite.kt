b00bc6713f14968b63f1cf475f38a1e64063f9ee
JonathanVandenEynden <47554911+JonathanVandenEynden@users.noreply.github.com>
2020-12-17 18:46:15 +0100

    Add Test Suite for all Test classes

---
+++ app/src/androidTest/java/com/hogentessentials1/essentials/ui/login/ui/login/AllTestsSuite.kt
@@ -0,0 +1,14 @@
+package com.hogentessentials1.essentials.ui.login.ui.login
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    AuthenticationTest::class,
+    ChangeInitiativeTest::class,
+    HomescreenTest::class,
+    LoginActivityTest::class,
+    RoadmapItemTest::class
+)
+class AllTestsSuite
