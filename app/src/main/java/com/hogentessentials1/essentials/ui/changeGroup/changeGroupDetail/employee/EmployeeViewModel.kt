69e88cda60ed7a453942898a7a668c8f474e69b5
ziggymoens <ziggy.moens@student.hogent.be>
2020-12-20 16:14:39 +0100

    ktlintformat

--- app/src/main/java/com/hogentessentials1/essentials/ui/changeGroup/changeGroupDetail/employee/EmployeeViewModel.kt
+++ app/src/main/java/com/hogentessentials1/essentials/ui/changeGroup/changeGroupDetail/employee/EmployeeViewModel.kt
@@ -2,12 +2,11 @@ package com.hogentessentials1.essentials.ui.changeGroup.changeGroupDetail.employ
 
 import androidx.lifecycle.ViewModel
 import com.hogentessentials1.essentials.data.model.Employee
-import com.hogentessentials1.essentials.data.model.Question
 
 /**
  * @author Jonathan Vanden Eynden Van Lysebeth
  */
 
 class EmployeeViewModel : ViewModel() {
-    lateinit var employee : Employee
+    lateinit var employee: Employee
 }
